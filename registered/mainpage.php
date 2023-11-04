1f45d6df80d4fea28fe9243305d3268e98e9405b
harvysdlc <harvysdlc@gmail.com>
2023-11-04 13:02:29 +0800

    login

--- registered/mainpage.php
+++ registered/mainpage.php
@@ -1,12 +1,12 @@
 <?php
 require '../server/config.php';
-if(!empty($_SESSION["id"])){
-    $id = $_SESSION["id"];
+if(!empty($_SESSION["email"])){
+    $email = $_SESSION["email"];
     $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE email = '$email'");
     $row = mysqli_fetch_assoc($result);
 }
 else{
-    header("Location: signin.php");
+    header("Location: login.php");
 }
 ?>
 
