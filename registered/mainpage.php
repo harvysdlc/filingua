f7e25785468da940a8c7c31b4ccff9c1330d797c
harvysdlc <harvysdlc@gmail.com>
2023-11-04 13:14:28 +0800

    changed

--- registered/mainpage.php
+++ registered/mainpage.php
@@ -1,31 +1,36 @@
 <?php
 require '../server/config.php';
+
 if(!empty($_SESSION["email"])){
     $email = $_SESSION["email"];
     $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE email = '$email'");
-    $row = mysqli_fetch_assoc($result);
-}
-else{
+    
+    if ($row = mysqli_fetch_assoc($result)) {
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+    } else {
+        // Handle the case where no user with the provided email was found.
+        header("Location: login.php");
+        exit; // Terminate the script to prevent further execution.
+    }
+} else {
     header("Location: login.php");
+    exit; // Terminate the script to prevent further execution.
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filingua - Learn</title>
-    <link rel="stylesheet" href="../">
-    <link rel="icon" type="image/png" href="../">
+    <!-- Your HTML head content -->
 
     <?php
     echo $email;
-    echo $first_name;
-    echo $last_name;
+    echo $first_name; // Echo the first name
+    echo $last_name;  // Echo the last name
     ?>
 </head>
 <body>
-    
+    <!-- Your HTML body content -->
 </body>
 </html>
