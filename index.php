a282c730fdd15954a0aab1fb2c3590d3eabb8a6d
harvysdlc <harvysdlc@gmail.com>
2023-11-10 11:33:52 +0800

    buttons

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
+<?php
+if(isset($_POST["signup"])){
+    header("Location: ../filingua/signup.php");
+}
+else if (isset($_POST["login"])){
+    header("Location: ../filingua/login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filingua</title>
-    <link rel="stylesheet" href="../">
+    <link rel="stylesheet" href="../filingua/css/index.css">
     <link rel="icon" type="image/png" href="../filingua/images/icon.jpg">
 </head>
 <body>
-    <a href="../filingua/signup.php"> get started </a>
+    <button type="submit" name="signup"> Create my filingua account  </button>
     <br>
-    <a href ="../filingua/login.php"> i already have an account </a>
+    <button type="submit" name="login"> I already have an account </button>
 </body>
 </html>
