ff7c73c08609e676c5d550c5b6c3d445aaf621c4
harvysdlc <harvysdlc@gmail.com>
2023-11-10 17:48:30 +0800

    bg

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ else if (isset($_POST["login"])){
     <link rel="icon" type="image/png" href="../filingua/images/icon.jpg">
     <style>
         body {
-            background-image: url(../filingua/images/mybg.gif);
+            background-image: url(../filingua/images/bg.gif);
             background-size: cover;
             background-repeat: no-repeat;
             height: 100vh;
