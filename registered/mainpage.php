59c6edc87db83b11418b958c61daaed6eb74e674
harvysdlc <harvysdlc@gmail.com>
2023-12-02 14:28:54 +0800

    edit

--- registered/mainpage.php
+++ registered/mainpage.php
@@ -25,7 +25,7 @@ if(!empty($_SESSION["email"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filingua - Learn</title>
-    <link rel="stylesheet" href="../filingua/css/mainpage.css">
+    <link rel="stylesheet" href="../../filingua/css/mainpage.css">
     <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
 </head>
 <body>
