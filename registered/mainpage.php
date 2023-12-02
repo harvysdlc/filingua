b0476e15581f371b93096d850c29cf86fffcf28e
harvysdlc <harvysdlc@gmail.com>
2023-12-02 17:04:50 +0800

    scroll

--- registered/mainpage.php
+++ registered/mainpage.php
@@ -31,23 +31,22 @@ if(!empty($_SESSION["email"])){
         body { background-color: #f4f0ec; }
     </style>
 </head>
-<body>
-    <div class="textimage">
-        <?php
-        // Insert your photo path below
-        $photoPath = "../../filingua/images/textimage.png";
-        ?>
-        
-        <img src="<?php echo $photoPath; ?>">
+<body>   
+    <div class="stage">
+        <div class="buttons">
+            <button onclick="window.location.href='../../filingua/registered/mainpage.php'">Learn</button>
+            <button onclick="window.location.href='../../filingua/registered/practice.php'">Practice</button>
+            <button onclick="window.location.href='../../filingua/registered/profile.php'">Profile</button>
+            <button onclick="window.location.href='../../filingua/registered/about.php'">About</button>
+            <button onclick="window.location.href='../../filingua/registered/logout.php'">Logout</button>
+        </div>
+        <div class="textimage">
+            <?php
+            // Insert your photo path below
+            $photoPath = "../../filingua/images/textimage.png";
+            ?>
+            <img src="<?php echo $photoPath; ?>">
+        </div>
     </div>
-
-    <div class="buttons">
-        <button onclick="window.location.href='../../filingua/registered/mainpage.php'">Learn</button>
-        <button onclick="window.location.href='../../filingua/registered/practice.php'">Practice</button>
-        <button onclick="window.location.href='../../filingua/registered/profile.php'">Profile</button>
-        <button onclick="window.location.href='../../filingua/registered/about.php'">About</button>
-        <button onclick="window.location.href='../../filingua/registered/logout.php'">Logout</button>
-    </div>
-    <div class="stage"></div>
 </body>
 </html>
