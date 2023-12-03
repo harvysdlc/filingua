c5dbf86963ad5f3d29d55eb8e10c49ca92d2e99b
harvysdlc <harvysdlc@gmail.com>
2023-12-03 14:24:40 +0800

    edit

--- registered/profile.php
+++ registered/profile.php
@@ -12,11 +12,11 @@ if (!empty($_SESSION["email"])) {
         $email = $row['email'];
         $profile_image_path = $row['profile_image_path'];
     } else {
-        header("Location: login.php");
+        header("Location: ../filingua/login.php");
         exit;
     }
 } else {
-    header("Location: login.php");
+    header("Location: ../filingua/login.php");
     exit;
 }
 ?>
@@ -52,7 +52,7 @@ if (!empty($_SESSION["email"])) {
             <h1><?php echo $first_name ?></h1>
             <p><?php echo $email ?></p>
             <img src="<?php echo $profile_image_path ? $profile_image_path : '../../filingua/images/default.png'; ?>" id="profile-pic">
-            <form action="../filingua/registered/upload_profile_pic.php" method="post" enctype="multipart/form-data">
+            <form action="../../filingua/registered/upload_profile_pic.php" method="post" enctype="multipart/form-data">
             <label for="input-file">Baguhin ang larawan</label>
             <input type="file" accept="image/jpeg, image/png, image/jpg" id="input-file" name="profile_pic">
             <input type="submit" value="Upload">
