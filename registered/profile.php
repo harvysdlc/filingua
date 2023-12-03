e1974e329ac1ba5e10b72ba21716ef1e3d8b4d5c
harvysdlc <harvysdlc@gmail.com>
2023-12-03 11:25:35 +0800

    buttons

---
+++ registered/profile.php
@@ -0,0 +1,48 @@
+<?php
+require '../server/config.php';
+
+if(!empty($_SESSION["email"])){
+    $email = $_SESSION["email"];
+    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE email = '$email'");
+    
+    if ($row = mysqli_fetch_assoc($result)) {
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+    } else {
+        // Handle the case where no user with the provided email was found.
+        header("Location: login.php");
+        exit; // Terminate the script to prevent further execution.
+    }
+} else {
+    header("Location: login.php");
+    exit; // Terminate the script to prevent further execution.
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filingua - Learn</title>
+    <link rel="stylesheet" href="../../filingua/css/profile.css">
+    <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
+    <style>
+        body { background-color: #ffbd59; }
+    </style>
+</head>
+<body>
+    <div class="stage">
+        <div class="logo">
+            <img src="../../filingua/images/textimage.png" alt="">
+        </div>
+        <div class="buttons">
+            <button onclick="window.location.href='../../filingua/registered/mainpage.php'">Learn</button>
+            <button onclick="window.location.href='../../filingua/registered/practice.php'">Practice</button>
+            <button onclick="window.location.href='../../filingua/registered/profile.php'">Profile</button>
+            <button onclick="window.location.href='../../filingua/registered/about.php'">About</button>
+            <button onclick="window.location.href='../../filingua/registered/logout.php'">Logout</button>
+        </div>
+    </div>    
+</body>
+</html>
