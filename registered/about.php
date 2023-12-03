904f6eb11a804df57bef63aee19ca817a514ddcc
harvysdlc <harvysdlc@gmail.com>
2023-12-03 11:27:12 +0800

    namings

--- registered/about.php
+++ registered/about.php
@@ -24,7 +24,7 @@ if(!empty($_SESSION["email"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filingua - Learn</title>
+    <title>Filingua - About</title>
     <link rel="stylesheet" href="../../filingua/css/about.css">
     <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
     <style>
--- registered/practice.php
+++ registered/practice.php
@@ -24,7 +24,7 @@ if(!empty($_SESSION["email"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filingua - Learn</title>
+    <title>Filingua - Practice</title>
     <link rel="stylesheet" href="../../filingua/css/practice.css">
     <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
     <style>
