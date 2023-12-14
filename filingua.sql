fa7c782cfec01caadae2b94db1bbb78c2fbc3235
harvysdlc <harvysdlc@gmail.com>
2023-12-14 22:38:18 +0800

    edits

--- filingua.sql
+++ filingua.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 06:02 AM
+-- Generation Time: Dec 10, 2023 at 01:47 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,16 +36,21 @@ CREATE TABLE `tb_user` (
   `password` varchar(50) NOT NULL,
   `progress` int(3) NOT NULL,
   `profile_image_path` varchar(255) DEFAULT NULL,
-  `AccType` varchar(20) NOT NULL
+  `AccType` varchar(20) NOT NULL,
+  `nationality` varchar(50) DEFAULT NULL,
+  `bio` varchar(50) DEFAULT NULL,
+  `language` varchar(50) NOT NULL,
+  `lesson` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`email`, `first_name`, `last_name`, `age`, `sex`, `password`, `progress`, `profile_image_path`, `AccType`) VALUES
-('harvysdlc@gmail.com', 'Harvy', 'Dela Cruz', 20, 'male', 'soriano22', 0, 'harvysdlc@gmail.com - 2023.12.07 - 02.38.01pm.jpg', 'developer'),
-('joshelland@gmail.com', 'Josh Elland', 'Sombrano', 21, 'male', 'ejsombrano', 0, 'joshelland@gmail.com - 2023.12.07 - 03.51.44pm.jpg', 'developer');
+INSERT INTO `tb_user` (`email`, `first_name`, `last_name`, `age`, `sex`, `password`, `progress`, `profile_image_path`, `AccType`, `nationality`, `bio`, `language`, `lesson`) VALUES
+('harvy@gmail.com', 'Javie', 'Soriano', 20, 'Lalaki', 'soriano22', 0, 'default.png', 'user', 'none', 'none', 'Tagalog', 'Aralin 6'),
+('harvysdlc@gmail.com', 'Harvy', 'Dela Cruz', 20, 'Lalaki', 'soriano22', 0, 'harvysdlc@gmail.com - 2023.12.09 - 07.29.14am.jpg', 'developer', 'Filipino', 'Wala ka na sa search list ko! Tandaan mo yan...', 'Tagalog', 'Aralin 5'),
+('joshelland@gmail.com', 'Josh Elland', 'Sombrano', 21, 'Lalaki', 'ejsombrano', 0, 'joshelland@gmail.com - 2023.12.08 - 12.56.39pm.jpg', 'developer', 'Filipino', 'Ashen Intrepid', '', '');
 
 --
 -- Indexes for dumped tables
---
+++ registered/sample.php
@@ -0,0 +1,111 @@
+<?php
+require '../server/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form  method="post">
+<?php 
+
+$questions = array();
+$lang_id = 1;
+$quiz_id = 1;
+$max_num_question = 3;
+
+for ($i = 1; $i <= $max_num_question; $i++) {
+
+    $question_num = rand(1, 5);
+
+    while (in_array($question_num, $questions)) {
+        $question_num = rand(1, 5);
+    }
+
+    if (!in_array($question_num, $questions)) {
+
+        array_push($questions, $question_num);
+
+        $stmt = $conn->prepare("SELECT * FROM questions_tbl WHERE quiz_id = ? AND lang_id = ? AND  ques_num = ?");
+        $stmt->bind_param('iii', $quiz_id, $lang_id, $question_num);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            while ($data = $result->fetch_assoc()) {
+                $question = $data['ques'];
+                $answer = $data['answer'];
+                $ques_num = $data['ques_num'];
+                echo "Question $i : $question ($ques_num) ";
+
+                $stmt_choices = $conn->prepare("SELECT * FROM choices_tbl WHERE quiz_id = ? AND lang_id = ? AND ques_num = ?");
+                $stmt_choices->bind_param('iii', $quiz_id, $lang_id, $ques_num);
+                $stmt_choices->execute();
+                $result_choices = $stmt_choices->get_result();
+
+                echo ' <select class="form-select"  name="answer_'.$i.'" >';
+
+                if ($result_choices->num_rows > 0) {
+                    while ($data_choices = $result_choices->fetch_assoc()) {
+                        $choices = $data_choices['choices'];
+                        echo '
+                            <option value="' . $choices . '" >' . $choices . '</option>
+                        ';
+                    }
+                    echo '</select> <br>';
+
+                }
+            }
+
+        }
+    }
+}
+
+?>
+<input type="submit" value="submit" name="submit_ans">
+
+<?php 
+
+if(isset($_POST['submit_ans'])){
+    for($i=1; $i<=$max_num_question; $i++){
+        $answer_id = 'answer_'.$i;
+        $answer = isset($_POST[$answer_id]) ? $_POST[$answer_id] : "1";
+        
+        $ques_num = $questions[$i-1];
+
+        $stmt = $conn->prepare("SELECT * FROM answer_tbl WHERE user_id = ? AND quiz_id = ? AND lang_id = ? AND ques_num = ?");
+        $stmt->bind_param('iiii', $user_id, $quiz_id, $lang_id, $ques_num);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if($result->num_rows > 0){
+            $stmt = $conn->prepare("UPDATE answer_tbl SET answer = ? WHERE user_id = ? AND quiz_id = ? AND lang_id = ? AND ques_num = ?");
+            $stmt->bind_param('siiii', $answer, $user_id, $quiz_id, $lang_id, $ques_num);
+            $stmt->execute();
+            //header("location: home.php");
+        }
+        else{
+            $stmt = $conn->prepare("INSERT INTO answer_tbl (user_id, quiz_id, lang_id, ques_num, answer) VALUES (?,?,?,?,?)");
+            $stmt->bind_param('iiiis', $user_id, $quiz_id, $lang_id, $ques_num, $answer);
+            $stmt->execute();
+
+            //header("location: home.php");
+        }
+
+    }
+}
+
+
+?>
+
+
+</form>
+</body>
+</html>
+
