884cefe91f49b19913180a4134972f7f9523c6c6
harvysdlc <harvysdlc@gmail.com>
2024-02-08 06:29:26 +0800

    update

--- block.php
+++ block.php
@@ -4,19 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blocked</title>
-    <link rel="stylesheet" href="../../filingua/css/mainpage.css">
+    <link rel="stylesheet" href="../../filingua/css/block.css">
     <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
-    <style>
-        body {
-            background-image: url(../../../filingua/images/block.jpg);
-            background-size: 100% 100%;
-            background-repeat: no-repeat;
-            height: 100vh;
-            margin: 0;
-        }
-    </style>
 </head>
 <body>
-    
+    <div class="message">
+        <img src="../filingua/images/why.png" style="width: 150px; height: 150px";>
+        <h2>Sorry! You cannot access this site!</h2>
+        <p>Upon checking, your account seems to be blocked by the administrator.<br><br>
+        <b>What Should I do?</b><br>Kindly send us an email to appeal [help.filingua@gmail.com] 
+    </div>
 </body>
 </html>
--- registered/systemupdate.php
+++ registered/systemupdate.php
@@ -4,19 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>System Update</title>
-    <link rel="stylesheet" href="../../filingua/css/mainpage.css">
+    <link rel="stylesheet" href="../../filingua/css/block.css">
     <link rel="icon" type="image/png" href="../../filingua/images/logo.png">
-    <style>
-        body {
-            background-image: url(../../../filingua/images/update.jpg);
-            background-size: 100% 100%;
-            background-repeat: no-repeat;
-            height: 100vh;
-            margin: 0;
-        }
-    </style>
 </head>
 <body>
-    
+    <div class="message">
+        <img src="../../filingua/images/why.png" style="width: 150px; height: 150px";>
+        <h2>Please come again later!</h2>
+        <p>We apologize for inconvenience, but our website is undergoing<br>
+        scheduled maintenance and updates. We will be back shortly!.<br><br>
+        <b>Thank You for your patience!</b>
+    </div>
 </body>
 </html>
